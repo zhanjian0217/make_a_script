555328f28e661cd39f33c7c74c22fe82c24e9970
Zhan <gian910147@gmail.com>
2023-05-28 01:48:24 +0800

    MY TEST

---
+++ db/migrate/20230527173150_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[7.1]
+  def change
+    create_table :comments do |t|
+      t.references :book, null: false, foreign_key: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
